1bca2f39c75b853d70273761d7ff99a214a65f07
Tanin Rojanapiansatith <sp_land@hotmail.com>
2015-08-15 23:08:56 +0700

    done choice.xml and change choice in changeView

--- app/src/main/java/silica/landtanin/ishiharaprac3/MainActivity.java
+++ app/src/main/java/silica/landtanin/ishiharaprac3/MainActivity.java
@@ -66,6 +66,20 @@ public class MainActivity extends AppCompatActivity {
 
         ishiharaPic.setImageResource(intDrawable[modelAnInt]);
 
+        //change choice
+        int intTimes[] = new int[10];
+        intTimes[0] = R.array.times1;
+        intTimes[1] = R.array.times2;
+        intTimes[2] = R.array.times3;
+        intTimes[3] = R.array.times4;
+        intTimes[4] = R.array.times5;
+        intTimes[5] = R.array.times6;
+        intTimes[6] = R.array.times7;
+        intTimes[7] = R.array.times8;
+        intTimes[8] = R.array.times9;
+        intTimes[9] = R.array.times10;
+
+
     } //Change View
 
     private void buttonController() {
